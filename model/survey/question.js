f97b8a494ff8ffece770ac6039069069d78841b9
water2027 <136900643+water2027@users.noreply.github.com>
2025-03-10 09:18:25 +0800

    db

--- model/survey/question.js
+++ model/survey/question.js
@@ -2,20 +2,24 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../../database/db');
 
 const Question = sequelize.define('question', {
-    question_id: {
-        type: DataTypes.UUID,
-        defaultValue: DataTypes.UUIDV4,
-        primaryKey: true,
-    },
-    question_type_id: {
-        type: DataTypes.UUID,
-        allowNull: false
-    },
-    is_required:{
-        type: DataTypes.BOOLEAN,
-        allowNull: false,
-        defaultValue: true
-    }
+	question_id: {
+		type: DataTypes.UUID,
+		defaultValue: DataTypes.UUIDV4,
+		primaryKey: true,
+	},
+	question_type_id: {
+		type: DataTypes.UUID,
+		allowNull: false,
+	},
+	target_id: {
+		type: DataTypes.UUID,
+		allowNull: false,
+	},
+	is_required: {
+		type: DataTypes.BOOLEAN,
+		allowNull: false,
+		defaultValue: true,
+	},
 });
 
 module.exports = Question;
